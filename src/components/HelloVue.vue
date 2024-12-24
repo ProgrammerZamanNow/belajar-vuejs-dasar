7d7047b9c39a9aa11032af2b7f37630e40a10d6b
khannedy <echo.khannedy@gmail.com>
2024-12-24 21:22:57 +0700

    Refactor HelloVue component for better reusability.
    Added a `data` object to centralize attribute bindings and replaced hardcoded class bindings with dynamic bindings using `v-bind`. Introduced a `buttonDisabled` property to manage button state dynamically. These changes improve maintainability and flexibility of the component.

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -2,12 +2,21 @@
 console.info("Hello Vue");
 
 const hello = "<h1>Hello Vue</h1>"
+
+const data = {
+  class: "hello",
+  id: "hello"
+};
+
+const buttonDisabled = false;
 </script>
 
 <template>
-  <h1 class="hello">{{hello}}</h1>
+  <h1 v-bind="data">{{hello}}</h1>
 
-  <div v-html="hello"></div>
+  <div :class="data.class" v-html="hello"></div>
+
+  <button :disabled="buttonDisabled">This is Button</button>
 </template>
 
 <style scoped>
