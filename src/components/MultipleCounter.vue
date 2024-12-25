37d8d9ce54ad5da4ddd848eade6558a3721ff98f
khannedy <echo.khannedy@gmail.com>
2024-12-25 23:10:27 +0700

    Update counter to support multiple instances
    Replaced Counter component with MultipleCounter to facilitate multiple counter displays. Adjusted Counter.vue to remove the counter name for a simpler UI and introduced MultipleCounter.vue to wrap multiple Counter components.

---
+++ src/components/MultipleCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import Counter from "./Counter.vue";
+</script>
+
+<template>
+  <h1>Counter Eko</h1>
+  <Counter/>
+  <h1>Counter Budi</h1>
+  <Counter/>
+</template>
+
+<style scoped>
+
+</style>
